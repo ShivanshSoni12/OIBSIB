eeb7dd21701f50c1c3268505217406f61d258db4
ShivanshSoni12 <115873396+ShivanshSoni12@users.noreply.github.com>
2022-12-25 23:33:37 +0530

    Create atminterface.java

---
+++ atminterface.java
@@ -0,0 +1,72 @@
+package ATM;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class atminterface {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int balance = 10000;
+
+        NumberFormat.getCurrencyInstance();
+        System.out.println("Welcome to Automated Teller Machine! \nKindly Enter your UserId :");
+        String Name = sc.nextLine();
+        System.out.println("Kindly Enter your 4-digit Pin :");
+        int Pin = sc.nextInt();
+
+        while (true) {
+            System.out.println("Welcome");
+            System.out.println("ATM Menu:");
+            System.out.println("Choose 1 for Withdraw");
+            System.out.println("Choose 2 for Deposit");
+            System.out.println("Choose 3 to Check Balance");
+            System.out.println("Choose 4 to Transfer Amount");
+            System.out.println("Choose 5 to EXIT");
+            System.out.print("Choose the operation you want to perform : ");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1 -> {
+                    System.out.println("Enter amount to be withdrawn :");
+                    int withdrawn = sc.nextInt();
+                    if (balance >= withdrawn) {
+                        balance -= withdrawn;
+                        System.out.println("New balance : " + balance + "\nPlease collect your cash and card !");
+                    } else {
+                        System.out.println("Insufficient Balance...");
+                    }
+                }
+                case 2 -> {
+                    System.out.println("Enter amount to be deposited :");
+                    int deposits = sc.nextInt();
+                    balance += deposits;
+                    System.out.println("New balance : " + balance);
+                }
+                case 3 -> System.out.println("Current Balance :" + balance);
+                case 4 -> {
+                    System.out.println("Enter Receiver's account number :");
+                    double other=sc.nextDouble();
+                    System.out.println("Enter amount to be transferred :");
+                    int amount= sc.nextInt();
+                    if(balance >= amount) {
+                        System.out.println("Enter your PIN :");
+                        int pin = sc.nextInt();
+                        if (pin == Pin) {
+                            balance -= amount;
+                            System.out.println("Amount has been transferred successfully from " + Name + " to account no. :" + other);
+                            System.out.println(" New Balance:" + balance);
+                        } else {
+                            System.out.println("Invalid PIN provided...");
+                        }
+                    }
+                    else{
+                        System.out.println("Insufficient Balance...");
+                    }
+                }
+                case 5 -> System.exit(0);
+            }
+            System.out.println("Thank You... Have a nice day !");
+        }
+    }
+
+}
